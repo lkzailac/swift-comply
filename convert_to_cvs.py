32c4443b74298be8e366c79fdddfe2ba12a0323b
Laura <lkzailac@gmail.com>
2021-09-08 11:51:20 -0400

    pandas

--- convert_to_cvs.py
+++ convert_to_cvs.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET
-
+import pandas as pd
 
 """
 Input: string (the location of the xml file to be converted)
@@ -12,9 +12,37 @@ Output: string (the location of the new csv file)
 tree = ET.parse("Facility.xml")
 root = tree.getroot()
 
-property_ids = {}
+properties_data_frame = {
+    "property_id":[],
+    "account_number":[],
+    "name":[],
+    "address1":[],
+    "city":[],
+    "state_prov":[],
+    "postal_code":[],
+    "primary_contact_id":[]
+}
 count = 0
 for property in root.iter("T_Facility"):
-    # print(property)
-    for id in property.iter("Facility_ID"):
-        print(id.text)
+
+    properties_data_frame["property_id"].append(property.find("Facility_ID").text)
+
+    if property.find("Facility_Account_Number") is not None:
+        properties_data_frame["account_number"].append(property.find("Facility_Account_Number").text)
+    else:
+        properties_data_frame["account_number"].append(None)
+
+    if property.find("Facility_Name") is not None:
+        properties_data_frame["name"].append(property.find("Facility_Name").text)
+    else:
+        properties_data_frame["name"].append(None)
+# print(properties_data_frame["account_number"])
+
+"""
+Covert the dictionary
+"""
+df = pd.DataFrame(properties_data_frame, columns = [
+    "property_id", "account_number", "name", "address1", "city", "state_prov",
+    "postal_code", "primary_contact_id"])
+
+print(df)
