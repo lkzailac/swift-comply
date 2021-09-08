f5c510367c39989366663cdb5f79e203ca6e1c87
Laura <lkzailac@gmail.com>
2021-09-08 10:12:57 -0400

    getting id's

--- convert_to_cvs.py
+++ convert_to_cvs.py
@@ -1,5 +1,5 @@
-import pandas as pd
-import Facility.xml from Facility.xml
+import xml.etree.ElementTree as ET
+
 
 """
 Input: string (the location of the xml file to be converted)
@@ -8,4 +8,13 @@ Output: string (the location of the new csv file)
         none (the function opens up the new file)
 """
 
-read_file = pd.read_csv(Facility.xml)
+# read_file = pd.read_csv(Facility.xml)
+tree = ET.parse("Facility.xml")
+root = tree.getroot()
+
+property_ids = {}
+count = 0
+for property in root.iter("T_Facility"):
+    # print(property)
+    for id in property.iter("Facility_ID"):
+        print(id.text)
